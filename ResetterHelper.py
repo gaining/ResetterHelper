feb83c1e7d0022b8d462e3bac6bf71ae733afe5a
gaining <gaining7@outlook.com>
2018-06-01 12:21:14 -0400

    updated resetterHelper

--- ResetterHelper.py
+++ ResetterHelper.py
@@ -3,8 +3,7 @@
 # Author Gaining AkA Jonathan Soivilus
 
 
-from PyQt5.QtWidgets import *
-from PyQt5 import QtCore, QtGui
+from PyQt5 import QtCore, QtGui, QtWidgets
 import subprocess
 import sys
 import os
@@ -13,37 +12,37 @@ import pwd
 import platform
 
 
-class ResetterHelper(QWidget):
+class ResetterHelper(QtWidgets.QWidget):
 
     def __init__(self, parent=None):
         super(ResetterHelper, self).__init__(parent)
         self.setWindowTitle("Resetter Helper v1.1")
         self.os_info = lsb_release.get_distro_information()
         os_information = '{} {}'.format(self.os_info['ID'], self.os_info['RELEASE'])
-        os_info_label = QLabel(os_information)
-        self.lbl2 = QLabel("File1")
-        self.lbl3 = QLabel("File2")
-        self.lbl4 = QLabel("")
-        self.btnCreateManifest = QPushButton("Make Manifest")
-        self.btnCreateUserlist = QPushButton("Make Userlist")
-        self.btnOpen1 = QPushButton("Open first file")
-        self.btnOpen2 = QPushButton("Open Second File")
-        self.btnCompare = QPushButton("Compare Files")
-        self.listView = QListView(self)
+        os_info_label = QtWidgets.QLabel(os_information)
+        self.lbl2 = QtWidgets.QLabel("File1")
+        self.lbl3 = QtWidgets.QLabel("File2")
+        self.lbl4 = QtWidgets.QLabel("")
+        self.btnCreateManifest = QtWidgets.QPushButton("Make Manifest")
+        self.btnCreateUserlist = QtWidgets.QPushButton("Make Userlist")
+        self.btnOpen1 = QtWidgets.QPushButton("Open First File")
+        self.btnOpen2 = QtWidgets.QPushButton("Open Second File")
+        self.btnCompare = QtWidgets.QPushButton("Compare Files")
+        self.listView = QtWidgets.QListView(self)
         self.model = QtGui.QStandardItemModel(self.listView)
-        self.msg = QMessageBox(self)
+        self.msg = QtWidgets.QMessageBox(self)
         self.euid = os.geteuid()
         palette = QtGui.QPalette()
         palette.setColor(QtGui.QPalette.Foreground, QtCore.Qt.blue)
         self.lbl4.setPalette(palette)
-        horizontalLayout = QHBoxLayout()
-        horizontalLayout2 = QHBoxLayout()
+        horizontalLayout = QtWidgets.QHBoxLayout()
+        horizontalLayout2 = QtWidgets.QHBoxLayout()
         horizontalLayout.addWidget(self.btnOpen1)
         horizontalLayout.addWidget(self.btnOpen2)
         horizontalLayout2.addWidget(self.lbl2)
         horizontalLayout2.addWidget(self.lbl3)
 
-        verticalLayout = QVBoxLayout(self)
+        verticalLayout = QtWidgets.QVBoxLayout(self)
         verticalLayout.addWidget(os_info_label)
         verticalLayout.addWidget(self.btnCreateManifest)
         verticalLayout.addWidget(self.btnCreateUserlist)
@@ -72,40 +71,35 @@ class ResetterHelper(QWidget):
         apt_locations = ('/usr/bin/apt', '/usr/lib/apt', '/etc/apt', '/usr/local/bin/apt')
         if not any(os.path.exists(f) for f in apt_locations):
             self.showMessage("Apt Not Found",
-                             "Apt could not be found, Your distro does not appear to be Debian based.",
-                             QMessageBox.Warning)
+                             "Apt could not be found, your distro does not appear to be Debian based.",
+                             QtWidgets.QMessageBox.Warning)
 
     def generateManifest(self):
         try:
-            QApplication.setOverrideCursor(QtCore.Qt.WaitCursor)
+            QtWidgets.QApplication.setOverrideCursor(QtCore.Qt.WaitCursor)
             p1 = subprocess.check_output(['dpkg', '--get-selections'])
             manifest = '_'.join((self.os_info['ID'], self.os_info['RELEASE'], \
-                                 os.environ.get('DESKTOP_SESSION'), platform.architecture()[0], ".manifest"))
+                                 os.environ.get('XDG_CURRENT_DESKTOP'), platform.architecture()[0], ".manifest"))
             with open(manifest, 'w') as output:
                 for line in p1.splitlines():
                     output.write(line.decode().split('\t', 1)[0] + '\n')
         except subprocess.CalledProcessError:
-            QApplication.restoreOverrideCursor()
+            QtWidgets.QApplication.restoreOverrideCursor()
             self.showMessage("Error",
                              "an error has occured while generating manifest",
-                             QMessageBox.Critical)
+                             QtWidgets.QMessageBox.Critical)
         else:
-            QApplication.restoreOverrideCursor()
+            QtWidgets.QApplication.restoreOverrideCursor()
             self.showMessage("Success",
                              "Your generated manifest can be found at {}".format(os.path.abspath(output.name)),
-                             QMessageBox.Information)
-
-    def dependencyInjector(self, man_list):
-        resetterdeps = ['pyqt4-dev-tools',
-                        'python-apt', 'python-aptdaemon', 'python-qt4', 'python-qt4-doc', 'libqt',
-                        'pyqt4-dev-tools', 'python-sip', 'python-mechanize', 'python-bs4', 'add-apt-key']
+                             QtWidgets.QMessageBox.Information)
 
     def generateUserlist(self):
         try:
-            QApplication.setOverrideCursor(QtCore.Qt.WaitCursor)
+            QtWidgets.QApplication.setOverrideCursor(QtCore.Qt.WaitCursor)
             cmd = subprocess.check_output(['bash', '-c', 'compgen -u'])
             userlist = '_'.join((self.os_info['ID'], self.os_info['RELEASE'], 'default-userlist',\
-            os.environ.get('DESKTOP_SESSION'), platform.architecture()[0]))
+            os.environ.get('XDG_CURRENT_DESKTOP'), platform.architecture()[0]))
 
             with open(userlist, 'w') as output:
                 for line in cmd.splitlines():
@@ -113,24 +107,24 @@ class ResetterHelper(QWidget):
                     if not self.user in line:
                         output.writelines(line + '\n')
         except subprocess.CalledProcessError as e:
-            QApplication.restoreOverrideCursor()
+            QtWidgets.QApplication.restoreOverrideCursor()
             self.showMessage("Error",
                              "an error has occured while getting users",
-                             QMessageBox.Critical, str(e))
+                             QtWidgets.QMessageBox.Critical, str(e))
         else:
-            QApplication.restoreOverrideCursor()
+            QtWidgets.QApplication.restoreOverrideCursor()
             self.showMessage("Success",
                              "Your distro's userlist can be found at {}".format(os.path.abspath(output.name)),
-                             QMessageBox.Information)
+                             QtWidgets.QMessageBox.Information)
 
     def openFile1(self):
-        file1, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "", "All Files (*)")
+        file1, _ = QtWidgets.QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "", "All Files (*)")
         if file1:
             self.file1 = file1
             self.lbl2.setText(str(file1).split('/')[-1])
 
     def openFile2(self):
-        file2, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "", "All Files (*)")
+        file2, _ = QtWidgets.QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "", "All Files (*)")
         if file2:
             self.file2 = file2
             self.lbl3.setText(str(file2).split('/')[-1])
@@ -153,7 +147,7 @@ class ResetterHelper(QWidget):
             pass
 
     def showMessage(self, title, message, icon, detail=None):
-        self.msg = QMessageBox()
+        self.msg = QtWidgets.QMessageBox()
         self.msg.setIcon(icon)
         self.msg.setWindowTitle(title)
         self.msg.setText(message)
@@ -163,7 +157,7 @@ class ResetterHelper(QWidget):
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     screen = ResetterHelper()
     screen.show()
     screen.checkForApt()
